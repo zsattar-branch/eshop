2b35fdcf0a3452f8a517f7de4e79761597f666d2
zainsattar18 <zainsattar18@gmail.com>
2020-08-03 17:51:11 -0400

    homepage completed with styles minus hover

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './MenuItem.scss'
+
+function MenuItem({ title, img, size }) {
+  return (
+    <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${img})`
+      }}
+    />    
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  )
+}
+
+export default MenuItem 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Homepage.scss'
+import '../components/Directory'
+import Directory from '../components/Directory'
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
+export default Homepage
