0268b2fc075383397783aeb40609091d74303f47
zainsattar18 <zainsattar18@gmail.com>
2020-08-04 14:24:16 -0400

    svg image upload & header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css'
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage.jsx'
 import Shop from './pages/shop/Shop.jsx'
+import Header from "./components/header/Header.jsx"
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route path="/shop" component={Shop}/>
