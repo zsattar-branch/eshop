35b7dc051c952bfdb4c843ed00b700a1f4843e35
zainsattar18 <zainsattar18@gmail.com>
2020-08-12 11:03:14 -0400

    remove action and type and reducer built and invoked it to the checkout item to remove item

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { clearItemFromCart } from '../../redux/cart/cart.actions'
+
 import './CheckoutItem.scss'
 
-function CheckoutItem({cartItem: {name, imageUrl, price, quantity}}) {
+function CheckoutItem({ cartItem,clearItem }) {
+  const { name, imageUrl, price, quantity } = cartItem
   return (
     <div className="checkout-item">
       <div className="image-container">
-        <img alt="item" src={imageUrl}/>
+        <img alt="item" src={imageUrl} />
       </div>
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">${price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={()=> clearItem(cartItem)}>&#10005;</div>
     </div>
   )
 }
 
-export default CheckoutItem
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect (null, mapDispatchToProps)(CheckoutItem)
