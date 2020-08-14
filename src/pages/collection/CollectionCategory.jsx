599b4c00dcb5997907c70ceb106210980985e175
zainsattar18 <zainsattar18@gmail.com>
2020-08-14 10:52:31 -0400

    changed the shop data to portray as an array and then changed the shop selector functions to to show that

--- src/pages/collection/CollectionCategory.jsx
+++ src/pages/collection/CollectionCategory.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { selectionCollection } from '../../redux/shop/shop.selector'
-
-import './CategoryCategory.scss'
+import './Collection.scss'
+import CollectionItem from '../../components/collection_item/Collection_Item'
 
 
 function CollectionCategory({ collection }) {
   console.log(collection)
+  const {title, items} = collection
   return (
     <div className="collection-page">
-      <h2>collection Page</h2>
-
+      <h2 className="title">{title}</h2>
+      <div className="items">{items.map(item => <CollectionItem key={item.id} item={item}/>)}
+      
+      </div>
     </div>
   )
 }
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,6 @@
 import { createSelector } from 'reselect'
 import memoize from 'lodash.memoize'
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
 
 const selectShopItem = state => state.shop
 
@@ -19,7 +12,5 @@ export const selectShopCollections = createSelector(
 export const selectionCollection = memoize ((collectionUrlParam) =>
   createSelector(
     [selectShopCollections],
-    collections =>
-      collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-))
+    collections => collections[collectionUrlParam]
+  ))
