d9496d397b8975fcf912d42a5304aaa4590d2415
zainsattar18 <zainsattar18@gmail.com>
2020-08-25 11:31:07 -0400

    changed the shop page so it shows .fetch method and .get method using a firebase api

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -20,13 +20,26 @@ class Shop extends Component {
     const { updateCollections } = this.props
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    // fetch('https://firestore.googleapis.com/v1/projects/zains-clothing/databases/(default)/documents/collections')
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections))
+
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap);
       this.setState({
         loading: false
       })
     })
+
+    
+  //   this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+  //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+  //     updateCollections(collectionsMap);
+  //     this.setState({
+  //       loading: false
+  //     })
+  //   })
   }
 
   render() {
