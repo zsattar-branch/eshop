f077dc687c5df8151822a7851c612a126209c937
zainsattar18 <zainsattar18@gmail.com>
2020-09-03 13:10:00 -0400

    media queries changed for mobile friendly

--- src/components/menuitems/MenuItem.jsx
+++ src/components/menuitems/MenuItem.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import './MenuItem.scss'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-function MenuItem({ title, imageUrl, size, linkUrl, history, match }) {
-  console.log(imageUrl)
-  return (
-    <div className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
-  )
-}
+import {
+  MenuItemContainer,
+  BackgroundImageContainer,
+  ContentContainer,
+  ContentTitle,
+  ContentSubtitle
+} from './Menu-items.styles';
 
-export default withRouter(MenuItem) 
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <MenuItemContainer
+    size={size}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <BackgroundImageContainer
+      className='background-image'
+      imageUrl={imageUrl}
+    />
+    <ContentContainer className='content'>
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
+    </ContentContainer>
+  </MenuItemContainer>
+);
+
+export default withRouter(MenuItem);
--- src/pages/Homepage.styles.jsx
+++ src/pages/Homepage.styles.jsx
@@ -4,7 +4,5 @@ export const HomepageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px 80px;
-
 `
 
